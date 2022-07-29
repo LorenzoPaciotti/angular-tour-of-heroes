21c488146157417848899e6467fcda2d78e4c2cb
Your Name <you@example.com>
2022-07-29 12:17:32 +0200

    c

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable,of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add(`hero.service: getHeroes() ${HEROES.length}`)
+    return heroes
+  }
+
+  getHero(id:number):Observable<Hero>{
+    const hero = HEROES.find(h=>h.id===id)!;
+    this.messageService.add(`hero.service: fetch hero ${id}`);
+    return of(hero)
+  }
+  constructor(private messageService:MessageService,http:HttpClient) { }
 }
