2e719f9699c2b0c9268b5abaf7be719c5586343e
Your Name <you@example.com>
2022-07-30 11:51:45 +0200

    c

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,19 +14,24 @@ export class HeroesComponent implements OnInit {
 
   heroes : Hero[] = []
 
-  /*selectedHero?: Hero
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-
-    this.messageService.add("heroes.component: hero ID ${hero.id}")
-  }*/
-
   getHeroes():void{
     this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes)
   }
 
-  /*constructor(private heroService: HeroService,private messageService:MessageService) { }*/
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
   constructor(private heroService:HeroService){}
 
   ngOnInit(): void {
